9bb41f81caa2a90bd40f2a4774900b42074e19b5
Pablo Mora Avalos <pamoav>
2014-03-16 18:19:54 +0100

    modificaciones pamoav 2

--- GitAux.java
+++ GitAux.java
@@ -15,7 +15,7 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
-	System.out.println("modificado por pamoav"); // alumno 2 do2 dos
+	System.out.println("modificado por pamoav"); // alumno 2
     }
 public void metodoAlumno3(){
         System.out.println("metodo 3");
@@ -23,7 +23,7 @@ public void metodoAlumno3(){
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
-	// modificaciones alumno 2 dos
-	System.out.println("Alumno 2 dos inserta esta sentencia");
+	// modificaciones alumno 2 
+	System.out.println("Alumno 2  inserta esta sentencia");
     }
 }
