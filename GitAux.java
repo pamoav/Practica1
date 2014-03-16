4239ff321230bd5b6ca13a56fc21910548bf7f36
Pablo Mora Avalos <pamoav14@uma.es>
2014-03-16 16:54:42 +0100

    Gitaux modificado

--- GitAux.java
+++ GitAux.java
@@ -1,12 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package prgitejemplo;
 
 /**
  *
- * @author bertoa
+ * @author Pablo
  */
 public class GitAux {
     
@@ -16,6 +13,9 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+    }
+public void metodoAlumno3(){
+        System.out.println("metodo 3");
     }
     
     public void metodoComunitario(){
